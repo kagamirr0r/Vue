aa9f930d182081e17aae80b0b701e3cc3f0a3356
kagamirr0r <45422771+kagamirr0r@users.noreply.github.com>
2020-11-19 11:10:31 +0900

    create vue app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input type="text" :value="msg" @input="changeMsg">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext } from 'vue';
+
+type Props = {
+  msg: string;
+}
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true,
+      default: 'defalut msg'
+    },
+  },
+  // emits: ['rewrite'],
+  setup(props: Props, context: SetupContext){
+    const changeMsg = (e: any) => {
+      context.emit('rewrite',e)
+    }
+    return {
+    changeMsg,
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg="msg" @rewrite="changeMsg"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const msg = ref('Hello Vue.js')
+    const changeMsg = (e: any) => {
+      msg.value = e.target.value
+    }
+    return {
+      msg,
+      changeMsg
+    }
+  },
+});
+</script>
