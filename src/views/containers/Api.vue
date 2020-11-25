cd0b5e7ad8771677ca89262f878793693963528c
kagamirr0r <45422771+kagamirr0r@users.noreply.github.com>
2020-11-25 17:31:54 +0900

    display bitcoin prace

--- src/views/containers/Api.vue
+++ src/views/containers/Api.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>{{ info }}</div>
+  <div
+  class="currency"
+  v-for="currency in info"
+  :key="currency"
+  v-cloak
+  >
+  {{ currency.description }}:
+  <span class="lighten">
+    <span v-html="currency.symbol"></span>{{ currencydecimal(currency.rate_float) }}
+  </span>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,15 +17,32 @@ import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 export default defineComponent({
   setup(){
-    const info: any = ref(null)
+    const info: any = ref(null);
+    const errored = ref(false);
+    const loading = ref(true);
     onMounted(() => {
       axios
         .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then(response => (info.value = response))
+        .then(response => (info.value = response.data.bpi))
+        .catch(error => {
+          console.log(error);
+          errored.value = true;
+          })
+        .finally(() => loading.value = false)
     })
+    const currencydecimal = (value: number) => {
+      return value.toFixed(2);
+    }
     return{
       info,
+      currencydecimal
     }
   },
 })
 </script>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
