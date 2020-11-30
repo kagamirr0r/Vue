12a72a197eda32a3254059d4be2040c667d8a8a7
kagamirr0r <45422771+kagamirr0r@users.noreply.github.com>
2020-11-30 17:28:29 +0900

    fix some Unnecessary

--- src/views/containers/Home.vue
+++ src/views/containers/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld :msg="msg" @rewrite="changeMsg"/>
+    <HelloWorld :msg="message" @rewrite="changeMsg"/>
     <p>This was made by context.emit</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, inject } from 'vue';
+import { defineComponent, ref} from 'vue';
 import HelloWorld from '@/views/presentationals/components/home/HelloWorld.vue';
 
 export default defineComponent({
@@ -16,12 +16,12 @@ export default defineComponent({
     HelloWorld,
   },
   setup() {
-    const msg = ref('Hello Vue.js')
+    const message = ref('Hello Vue.js')
     const changeMsg = (e: any) => {
-      msg.value = e.target.value
+      message.value = e.target.value
     }
     return {
-      msg,
+      message,
       changeMsg,
     }
   },
--- src/views/presentationals/components/home/HelloWorld.vue
+++ src/views/presentationals/components/home/HelloWorld.vue
@@ -13,7 +13,6 @@ type Props = {
 }
 
 export default defineComponent({
-  name: 'HelloWorld',
   props: {
     msg: {
       type: String,
@@ -31,21 +30,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
